0e8312717cc5ae6fd47f5a8a732c93487e156e92
tpipan <tunde.pipan@gmail.com>
2023-06-16 14:17:01 +0200

    Tema 2 rezolvata

---
+++ src/main/java/ro/sit/homework/LeapYear.java
@@ -0,0 +1,23 @@
+package ro.sit.homework;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 1900;
+        int days = 0;
+
+
+        for (int i = 1900; i >= 1900 && i <= 2016; i++) {
+            if ((i%4==0) || ((i%100==0)&&(i%400==0))) {
+                days = 29;
+            }
+            else{
+            days = 28;
+            }
+            System.out.println("Numarul de zile din luna Februarie este pentru anul " + i + " este " + days);
+        }
+    }
+}
+
+
+
+
